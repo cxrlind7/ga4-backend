8225390a6803b857910cdba0428b78f1bd11e16c
cxrlind0111 <34328150+cxrlind7@users.noreply.github.com>
2025-12-03 01:22:39 -0600

    Refactor server.js to use local getAnalyticsData

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import {
   getRoutePageViews,
   getPersonPageViews,
   getBlogEventBreakdown,
-} from '../server/getAnalyticsData.js'
+} from './getAnalyticsData.js'
 
 const app = express()
 app.use(cors())
@@ -84,3 +84,4 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
+
